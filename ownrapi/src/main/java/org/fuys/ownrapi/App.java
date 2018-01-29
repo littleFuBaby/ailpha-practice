00a70ad230b04d3af4cf270c77e62e7326b88718
'littleFuBaby' <'520fys008@163.com'>
2018-01-30 01:04:37 +0800

    commit own remote api project first time

---
+++ ownrapi/src/main/java/org/fuys/ownrapi/App.java
@@ -0,0 +1,13 @@
+package org.fuys.ownrapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

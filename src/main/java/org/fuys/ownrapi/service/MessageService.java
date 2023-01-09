3c845b87c42a912076b4306ed1a4883a2ea15480
andy <yunsonglife@sina.com>
2023-01-09 15:20:31 +0800

    move dir

---
+++ src/main/java/org/fuys/ownrapi/service/MessageService.java
@@ -0,0 +1,12 @@
+package org.fuys.ownrapi.service;
+
+public interface MessageService {
+	
+	/**
+	 * Define message echo
+	 * @param message
+	 * @return
+	 */
+	public String echo(String message);
+
+}
